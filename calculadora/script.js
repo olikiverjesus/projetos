ea5df19757002fafc5f8eee393c5c1db56ef9e19
olikiverjesus <149892232+olikiverjesus@users.noreply.github.com>
2024-08-12 23:15:36 -0300

    Changes

--- calculadora/script.js
+++ calculadora/script.js
@@ -7,6 +7,11 @@ function appendToDisplay(value) {
     document.getElementById('display').value = '';
   }
   
+  function deleteLastChar() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+  }
+  
   function calculateResult() {
     const display = document.getElementById('display');
     try {
